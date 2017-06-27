78b42974d7d94a41f8c1fe294aac617f27047a17
Jacob Thomas <jcthomas00@gmail.com>
2017-06-26 23:45:47 -0500

    HW3 v2

---
+++ Hangman-Game/assets/javascript/game - Copy.js
@@ -0,0 +1,96 @@
+var guessedLetters = [];
+var artists = [];
+var artistsDisplayed, artistToGuess; 
+
+//Largely copied from W3School's documents on XMLHttpRequest object and XML Parsing
+// https://www.w3schools.com/xml/xml_parser.asp
+var XMLRequest = new XMLHttpRequest();
+XMLRequest.open("GET", "https://api.7digital.com/1.2/artist/bytag/top?tags=pop&pageSize=50&country=ww&oauth_consumer_key=7dh7vfe3ydzu&", true)
+XMLRequest.send();
+
+XMLRequest.onreadystatechange = function () {
+	if (this.readyState == 4 && this.status == 200) {
+		var artistXML = XMLRequest.responseXML.getElementsByTagName("sortName");
+		for(var i = 0; i < artistXML.length; i++)	
+			artists.push(artistXML[i].childNodes[0].nodeValue.toUpperCase());		
+		newGame();
+	}
+};
+
+//What to do when user presses a key on the page
+document.onkeypress = function hangmanFunction(event) {
+	//get the keypress and make it uppercase for easy comparison
+	var keyChar = event.key.toUpperCase();
+	//do stuff if user pressed a string
+	if ((keyChar.match(/[A-Z]/))){
+		//do stuff if this is a new guess
+		if(guessedLetters.indexOf(keyChar) < 0) {
+			guessedLetters.push(keyChar)
+			//do stuff if the guess is in the artistToGuess
+			if (artistToGuess.includes(keyChar)){
+				var index = artistToGuess.indexOf(keyChar);
+				var subString = artistToGuess;
+				do {
+					artistsDisplayed[index] = keyChar;
+					subString = (subString.substr(subString.indexOf(keyChar)+1));
+					index += subString.indexOf(keyChar) + 1;
+				} while(subString.indexOf(keyChar) > -1);
+				if(artistsDisplayed.join("")===artistToGuess)
+					problemSolved(artistToGuess);
+			}
+		document.getElementById("wordToGuess").textContent = artistsDisplayed.join("");
+		document.getElementById("guessedWords").textContent = guessedLetters.join(" ");
+		}
+	}
+}
+
+//function that takes in a word and returns it as an array of characters
+function makeHangWord(mysteryWord) {
+	var unsolved = [];
+	//create a character array with blanks from the mysteryWord
+	//user only guesses letters, any other characters are already shown
+	for (var i = 0; i < mysteryWord.length; i++) {
+		if(mysteryWord[i].match(/[A-Z]/)){
+			unsolved.push("*");
+		}
+		else{
+			unsolved.push(mysteryWord[i]);
+		}
+	}
+	return unsolved;
+}
+
+//function to do stuff when correct arist is identified
+function problemSolved(artist) {
+	var videoXML = new XMLHttpRequest();
+	videoXML.open("GET", 'https://www.googleapis.com/youtube/v3/search?part=' +
+		'snippet&maxResults=1&videoEmbeddable=true&q=' + artist + '&type=video&key=AIzaSyDPYQ9lfI14PArEqd1GpWGbZ7IudqDt3sA', 
+		true);
+	videoXML.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200){
+		// 	console.log("video end");
+			var video = JSON.parse(videoXML.responseText);
+			// console.log(video.items[0].id.videoId);
+			document.getElementById("videoSpot").innerHTML = '<iframe ' +
+			'width="560" height="315" src="https://www.youtube.com/embed/' + video.items[0].id.videoId + 
+			'?autoplay=1"></iframe>';
+		}
+	}
+	videoXML.send();
+
+	alert("Problem Solved");
+	newGame();
+}
+
+//function to get a new element from our array and write it to html
+function newGame() {
+	//Pick a random word from my array
+	artistToGuess = artists[Math.floor(Math.random()*artists.length)];
+	artistsDisplayed = makeHangWord(artistToGuess);
+	guessedLetters.length = 0;
+
+	//push the array to the document without commas
+	document.getElementById("wordToGuess").textContent = artistsDisplayed.join("");
+	document.getElementById("guessedWords").textContent = guessedLetters.join(" ");
+	console.log(artistToGuess);
+}
--- Hangman-Game/assets/javascript/game.js
+++ Hangman-Game/assets/javascript/game.js
@@ -1,19 +1,114 @@
-var guessedLetters = [];
-var artists = [];
-var artistsDisplayed, artistToGuess; 
+var artists = []; //array of dynamically populated artists
+var MAX_GUESSES = 9; //pre-defined guess-limit
+var bingSound = new sound("assets/sounds/ding.wav"); //location of sound asset
 
-//Largely copied from W3School's documents on XMLHttpRequest object and XML Parsing
-// https://www.w3schools.com/xml/xml_parser.asp
+function HHHangman()  {
+	var guessedLetters = [];
+	var artistsDisplayed, artistToGuess, incorrectGuesses;
+	//method to get a new element from our array and write it to html
+	this.newGame = function() {
+		//pick a random word from artists array
+		artistToGuess = artists[Math.floor(Math.random()*artists.length)];
+		artistsDisplayed = this.makeHangWord(artistToGuess);
+		guessedLetters.length = 0;
+		incorrectGuesses = MAX_GUESSES;
+
+		//push the array to the document without commas
+		pushToPage("wordToGuess", artistsDisplayed.join(""));
+		pushToPage("guessedWords", guessedLetters.join(" "));
+		pushToPage("remainingGuesses", incorrectGuesses);
+	},
+
+	//method that takes in a word and returns it as an array of characters
+	this.makeHangWord = function(mysteryWord) {
+		var unsolved = [];
+		//create a character array with * from the mysteryWord
+		//user only guesses letters, any other characters are already shown
+		for (var i = 0; i < mysteryWord.length; i++) {
+			if(mysteryWord[i].match(/[A-Z]/)){
+				unsolved.push("*");
+			}
+			else{
+				unsolved.push(mysteryWord[i]);
+			}
+		}
+		return unsolved;
+	},
+
+	//method to do stuff when correct arist is identified
+	this.problemSolved = function(artist, gaveUp) {
+		var videoXML = new XMLHttpRequest();
+		videoXML.open("GET", 'https://www.googleapis.com/youtube/v3/search?part=' +
+			'snippet&maxResults=1&videoEmbeddable=true&q=' + artist + '&type=video&key=AIzaSyDPYQ9lfI14PArEqd1GpWGbZ7IudqDt3sA', 
+			true);
+		videoXML.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200){
+				var video = JSON.parse(videoXML.responseText);
+				if (gaveUp === false)
+					pushToHTML("result", '<span class="alert alert-success">You got it!' +
+					' The answer was ' + artist + '</span>');
+				else 
+					pushToHTML("result", '<span class="alert alert-danger"> The answer ' +
+					'was ' + artist + '. Try again!</span>');
+				pushToHTML("videoSpot", '<br><iframe width="100%" height="300px" ' +
+					'src="https://www.youtube.com/embed/' + 
+					video.items[0].id.videoId + '?autoplay=1"></iframe>');
+			}
+		}
+		videoXML.send();
+		this.newGame();
+	}
+	this.giveUp = function () {
+		this.problemSolved(artistToGuess, true);
+	},
+
+	//Function to process user input
+	this.analyzeInput = function (keyChar) {
+		//do stuff if this is a new guess
+		if(guessedLetters.indexOf(keyChar) < 0) {
+			guessedLetters.push(keyChar);
+			pushToPage("guessedWords", guessedLetters.join(" "));
+			//do stuff if the guess is in the artistToGuess
+			if (artistToGuess.includes(keyChar)){
+				bingSound.play();
+				var index = artistToGuess.indexOf(keyChar);
+				var subString = artistToGuess;
+				do {
+					artistsDisplayed[index] = keyChar;
+					subString = (subString.substr(subString.indexOf(keyChar)+1));
+					index += subString.indexOf(keyChar) + 1;
+				} while(subString.indexOf(keyChar) > -1);
+				pushToPage("wordToGuess", artistsDisplayed.join(""))
+
+				//All letters correctly guessed
+				if(artistsDisplayed.join("")===artistToGuess) {
+					this.problemSolved(artistToGuess, false);
+				}
+			}
+			else {
+				incorrectGuesses--;
+
+			}
+			pushToPage("remainingGuesses", incorrectGuesses)
+			if (incorrectGuesses <= 0)
+				this.giveUp();
+		}
+	
+	}
+}//end of HHHangman object
+
+var myHangman = new HHHangman();
+//Get a list of top 50 hip-hop artists using 7Digital API
 var XMLRequest = new XMLHttpRequest();
-XMLRequest.open("GET", "https://api.7digital.com/1.2/artist/bytag/top?tags=pop&pageSize=50&country=ww&oauth_consumer_key=7dh7vfe3ydzu&", true)
+XMLRequest.open("GET", "https://api.7digital.com/1.2/artist/bytag/top?tags=hip-hop/rap,pop&pageSize=50&country=ww&oauth_consumer_key=7dh7vfe3ydzu&", true)
 XMLRequest.send();
-
 XMLRequest.onreadystatechange = function () {
 	if (this.readyState == 4 && this.status == 200) {
 		var artistXML = XMLRequest.responseXML.getElementsByTagName("sortName");
 		for(var i = 0; i < artistXML.length; i++)	
-			artists.push(artistXML[i].childNodes[0].nodeValue.toUpperCase());		
-		newGame();
+			artists.push(artistXML[i].childNodes[0].nodeValue.toUpperCase());	
+		//Start the game, now that our array is populated	
+		myHangman.newGame();
 	}
 };
 
@@ -23,74 +118,37 @@ document.onkeypress = function hangmanFunction(event) {
 	var keyChar = event.key.toUpperCase();
 	//do stuff if user pressed a string
 	if ((keyChar.match(/[A-Z]/))){
-		//do stuff if this is a new guess
-		if(guessedLetters.indexOf(keyChar) < 0) {
-			guessedLetters.push(keyChar)
-			//do stuff if the guess is in the artistToGuess
-			if (artistToGuess.includes(keyChar)){
-				var index = artistToGuess.indexOf(keyChar);
-				var subString = artistToGuess;
-				do {
-					artistsDisplayed[index] = keyChar;
-					subString = (subString.substr(subString.indexOf(keyChar)+1));
-					index += subString.indexOf(keyChar) + 1;
-				} while(subString.indexOf(keyChar) > -1);
-				if(artistsDisplayed.join("")===artistToGuess)
-					problemSolved(artistToGuess);
-			}
-		document.getElementById("wordToGuess").textContent = artistsDisplayed.join("");
-		document.getElementById("guessedWords").textContent = guessedLetters.join(" ");
-		}
+		myHangman.analyzeInput(keyChar);
 	}
 }
 
-//function that takes in a word and returns it as an array of characters
-function makeHangWord(mysteryWord) {
-	var unsolved = [];
-	//create a character array with blanks from the mysteryWord
-	//user only guesses letters, any other characters are already shown
-	for (var i = 0; i < mysteryWord.length; i++) {
-		if(mysteryWord[i].match(/[A-Z]/)){
-			unsolved.push("*");
-		}
-		else{
-			unsolved.push(mysteryWord[i]);
-		}
-	}
-	return unsolved;
+//Function to send text to the HTML doc
+var pushToPage = function(elemendId, textInput) {
+	document.getElementById(elemendId).textContent = textInput;
 }
 
-//function to do stuff when correct arist is identified
-function problemSolved(artist) {
-	var videoXML = new XMLHttpRequest();
-	videoXML.open("GET", 'https://www.googleapis.com/youtube/v3/search?part=' +
-		'snippet&maxResults=1&videoEmbeddable=true&q=' + artist + '&type=video&key=AIzaSyDPYQ9lfI14PArEqd1GpWGbZ7IudqDt3sA', 
-		true);
-	videoXML.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200){
-		// 	console.log("video end");
-			var video = JSON.parse(videoXML.responseText);
-			// console.log(video.items[0].id.videoId);
-			document.getElementById("videoSpot").innerHTML = '<iframe ' +
-			'width="560" height="315" src="https://www.youtube.com/embed/' + video.items[0].id.videoId + 
-			'?autoplay=1"></iframe>';
-		}
-	}
-	videoXML.send();
-
-	alert("Problem Solved");
-	newGame();
+//Function to send HTML code to the HTML doc
+var pushToHTML = function(elemendId, htmlInput) {
+	document.getElementById(elemendId).innerHTML = htmlInput;
 }
 
-//function to get a new element from our array and write it to html
-function newGame() {
-	//Pick a random word from my array
-	artistToGuess = artists[Math.floor(Math.random()*artists.length)];
-	artistsDisplayed = makeHangWord(artistToGuess);
-	guessedLetters.length = 0;
+//Function activated when user clicks 'Give Up' button
+function giveUp() {
+	myHangman.giveUp();
+}
 
-	//push the array to the document without commas
-	document.getElementById("wordToGuess").textContent = artistsDisplayed.join("");
-	document.getElementById("guessedWords").textContent = guessedLetters.join(" ");
-	console.log(artistToGuess);
+//copied from w3schools to create ding sound
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }
 }
