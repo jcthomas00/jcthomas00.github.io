1bea1031066efd3c3c5d4a562a922e5f51bfee3a
Jacob Thomas <jcthomas00@gmail.com>
2017-07-30 17:02:52 -0500

    added final comments

--- RPS-Multiplayer/assets/js/main.js
+++ RPS-Multiplayer/assets/js/main.js
@@ -1,5 +1,5 @@
 var rPs = {
-
+    //firebase config info
     config : {
         apiKey: "AIzaSyDUeIymic_2E-_TT8r36nY4BkREZD2S8VU",
         authDomain: "rockpapersci.firebaseapp.com",
@@ -23,9 +23,8 @@ var rPs = {
         rPs.clickHandlers();                        //activate listeners
     },
     clickHandlers: function() {
-
+        //we have a new player name.
         $(".ready").on("click", function(e) {
-            console.log(rPs.numPlayers);
             var curPlayer = $(e.target).attr("data-player");
             var enteredText = $('#' + curPlayer + ' .name').val().trim();
             if (enteredText == "") {                //player name can't be blank
@@ -38,7 +37,7 @@ var rPs = {
                 var connectionsRef = database.ref("/players/"+rPs.playerDesignation);
                 connectionsRef.set(rPs.players[rPs.playerDesignation]);
                 //remove player from DB if they disconnect
-                connectionsRef.onDisconnect(
+                connectionsRef.onDisconnect(        //delete user on disconnect
                     function () {
                         messagesDB.push(new rPs.Message(enteredText +
                             " has left the building.", 'admin'));
@@ -50,56 +49,64 @@ var rPs = {
         //When player picks R, P or S
         $(".move").on("click", function(e) {
             var curPlayer = $(e.target.offsetParent).attr('id');
+            //add the move to player's 'last move' property
             rPs.players[curPlayer].lastMove = $(e.target).attr("data-move");
+            //hide other moves until this round is over
             $('#' + rPs.playerDesignation + ' .move').not($(e.target)).hide();
-            playerDB.set(rPs.players);
+            playerDB.set(rPs.players);              //send players to firebase
         });
 
         playerDB.on('value', function(data){
-            if(data.val()==null){
-                players = { player1: null, player2: null };
+            if(data.val()==null){                   //delete local data if nothing in DB
+                players = { 
+                    player1: null, 
+                    player2: null 
+                };
             }else{
-                rPs.players = data.val();
+                rPs.players = data.val();           //lets get DB changes
                 rPs.numPlayers = Object.keys(data.val()).length;
-                if(rPs.numPlayers>1){
+                if(rPs.numPlayers>1){               //we have 2 playes
                     let moveFlag = true;
-                    for(key in rPs.players){
+                    for(key in rPs.players){        //check if both players have made moves
                         if (rPs.players[key].lastMove === ""){
-                            moveFlag=false;
+                            moveFlag=false;         
                             break;
                         }
                     }
                     if (moveFlag) {
-                            rPs.getResult();
+                            rPs.getResult();        //get result
                     }
                 }     
-                rPs.updatePlayers();
+                rPs.updatePlayers();                //update UI
             }
         }, function(error){alert("Error! Cannot compute!")});
 
         /* Chatroom Functionality */
-        messagesDB.on('child_added', function(data){
-            rPs.showMessage(data.val());
-        }, function(e){
+        messagesDB.on('child_added',                //we have a new msg
+            function(data){    
+                rPs.showMessage(data.val());        //send to UI
+            }, function(e){
             alert("You have been disconnected");
         });
 
         $('#send').on('click', function(e){
-            e.preventDefault();
+            e.preventDefault();                     //prevent page refresh
             let message = $('#message-input').val().trim();
             if(message===""){
-                alert("Cannot send blank message")
+                alert("Cannot send blank message")  //don't allow blank messages
             } else{
-                messagesDB.push(new rPs.Message(message));
+                messagesDB.push(
+                    new rPs.Message(message));      //make message object and send to DB
             }
-            $('#message-input').val("");
+            $('#message-input').val("");            //clear message box in UI
         });
         /* End Chatroom Functionality */
     },//end of clickHandlers
 
     updatePlayers: function() {
         $('.player-title').html("Player Name: ")
-        $(' .form-group').slideDown();
+        $(' .form-group').slideDown();              //show name input
+        //update UI with all player info and disable R P S buttons for now
         for (curPlayer in rPs.players){
             if (rPs.players[curPlayer] != null){
                 $('#' + curPlayer + ' .player-title').html("Player Name: " + 
@@ -114,13 +121,14 @@ var rPs = {
             }
         }
     },
-
-    //disable all buttons except the move button for current player
+    //function to enable move buttons when we have 2 players
     checkPlayers: function() {
         for (key in rPs.players) {
             if (rPs.players[key] == null) {
+                //player missing - disable all buttons
                 $("#" + key + " .moves :button").attr('disabled', true);
             } else if (rPs.numPlayers > 1) {        //we  have our 2 players
+                //only enable move buttons for current player
                 $("#" + rPs.playerDesignation + " .moves :button").removeAttr('disabled');
                 $('#' + key + ' .move-text').html("Make your move:");
                 $('#' + key + ' .player-title').html("Player Name: " + rPs.players[key].name);
@@ -128,10 +136,12 @@ var rPs = {
         }
     }, //checkPlayers function
 
+    //make all buttons visible
     refresh: function(){
         $('.moves :button').show();
     },
 
+    //calculate who won the round based on each player's 'last move' property
     getResult: function() {
         rPs.refresh();
         if(rPs.players['player1'].lastMove === rPs.players['player2'].lastMove){
@@ -157,6 +167,7 @@ var rPs = {
         }
     },
 
+    //called by getResult to update wins and losses and show in UI
     winner: function(aPlayer){
         if (aPlayer === 'tied'){
             $('.result').html("You both tied with " + 
@@ -176,9 +187,10 @@ var rPs = {
                 }
             }
         }
-        rPs.refresh();
-        playerDB.set(rPs.players);
+        rPs.refresh();                              //enable move buttons
+        playerDB.set(rPs.players);                  //send new data to firebase
     },
+    //how to style the message
     showMessage: function(message){
         let style="";
         switch (message.sender){
@@ -194,21 +206,24 @@ var rPs = {
             default:
                 style = 'right';
         }
+        //add message to our messages list
         $('.messages ul').append($('<li class="li-' + style +'">').html(
             '<span class="li-message">' + message.message + '</span>' +
             '<span class="li-username">- ' + message.sender + " | " + 
             message.time + '</span>'));
+        //scroll to the bottom
         $(".messages").animate({scrollTop: $(".messages").prop("scrollHeight")}, 1000);
     },
 
+    //player class to hold player data
     Player: function(name) {
-
         this.name = name;
         this.wins = 0;
         this.losses = 0;
         this.ties = 0;
         this.lastMove = "";
     },
+    //message class to create objects for each message
     Message: function(chat, sender = 'anonymous') {
         if(sender!='admin'){
             if (rPs.playerDesignation != ""){
@@ -224,6 +239,6 @@ var rPs = {
 }//rPs Object
 
 $(document).ready(function() {
-    rPs.init();
-}); //document.ready
+    rPs.init();                     //lets start the game when the page is loaded
+});
 
