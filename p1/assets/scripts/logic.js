b14ce3bb83dfff99522d74b68388271a9fc06645
Jacob Thomas <jcthomas00@gmail.com>
2017-08-06 17:01:45 -0500

    adding profile bio functionality 16

--- p1/assets/scripts/logic.js
+++ p1/assets/scripts/logic.js
@@ -323,8 +323,8 @@ WhoUB.prototype.analyzezPersonality = function(e) {
 				}else {
 					bio += PERSONALITY_GRID[aPersonality][facet[facetIndex].name][0];
 				}
-				bio += "<br>";
 			}
+			bio += "<br>";
 		}
 		this.profileText.html(bio);
 		
